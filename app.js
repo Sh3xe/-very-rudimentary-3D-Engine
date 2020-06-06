4f66f18f29500371744c789dcae17ccb8729348a
Arnaud Pelissier <qwerlyz666@gmail.com>
2020-06-06 16:35:54 +0200

    app advencment

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
-let points = [[0, 0, 0], [0, 1, 0], [1, 0, 0], [1, 1, 0], [0, 0, 1], [0, 1, 1], [1, 0, 1], [1, 1, 1]];
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Vec3{
+    constructor(x, y, z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
+class Matrix3{
+    constructor(p, q, r){
+        this.m = [p, q, r];
+    }
+}
+
+class CordinateSpace{
+    constructor(location, matrix){
+        this.location = location;
+        this.matrix = matrix;
+    }
+}
+
+
+let points = [[100, 100, 100], [100, 200, 100], [200, 100, 100], [200, 200, 100], [100, 100, 200], [100, 200, 200], [200, -100, 200], [200, 200, 200]];
 // dx / z
 
 function animate(){
@@ -9,9 +34,13 @@ function animate(){
     for(let p of points){
         ctx.beginPath();
         ctx.fillStyle = "black";
-        ctx.arc( p[0] / p[2] ,p[1] / p[2] , 10, 0, Math.PI*2 );
+        let x = (p[0] / p[2])*100;
+        let y = (p[1] / p[2])*100;
+        ctx.arc( x , y, 5, 0, Math.PI*2 );s
         ctx.fill();
     }
 
     requestAnimationFrame(animate);
 }
+
+animate();
